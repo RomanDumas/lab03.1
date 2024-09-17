7a1f7991025aa55b0b2410475290d6ea11550c37
RomanDumas <dumasroman1011@gmail.com>
2024-09-18 00:10:50 +0300

    second commit

--- LR_3.1/LR3.1.cpp
+++ LR_3.1/LR3.1.cpp
@@ -12,18 +12,20 @@ int main() {
 
 	if (x < 0 || x == 0) {
 		A = log(cos(x)) + x * x * x * x * x;
-		
+		cout << "1" << endl;							//перевырка першого способу
 	}
 	if (x > 0 && x < 3) {
 		A = cos(1 + log(x)) / sin(1 + log(x));
-		
+		cout << "2" << endl;
 	}
 	if (x == 3) {
 		A = cos(1 + log(x)) / sin(1 + log(x));
-		
+		cout << "2" << endl;
 	}
+
 	if (x > 3) {
 		A = 12 * x - x * x * x * x * x * x * x * x;
+		cout << "3" << endl;
 		
 	}
 	y = 2 + 6 * x + A;
@@ -32,17 +34,25 @@ int main() {
 
 	//2 спосіб
 
-	if (x < 0 || x == 0)
+	if (x < 0 || x == 0) {
 		A = log(cos(x)) + x * x * x * x * x;
+		cout << "11" << endl;							//перевірка другого способу
+	}
 	else
-		if (x > 0 && x < 3)
+		if (x > 0 && x < 3) {
 			A = cos(1 + log(x)) / sin(1 + log(x));
+			cout << "22" << endl;
+		}
 		else 
-			if (x == 3) 
+			if (x == 3) {
 				A = cos(1 + log(x)) / sin(1 + log(x));
+				cout << "22" << endl;
+			}
 			else
-				if (x > 3)
+				if (x > 3) {
 					A = 12 * x - x * x * x * x * x * x * x * x;
+					cout << "33" << endl;
+			}
 	y = 2 + 6 * x + A;
 
 	cout << "2) y = " << y << endl;
